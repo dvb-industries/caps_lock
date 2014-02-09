2dfc4521863a09b1e529d2a4073d6333d28f33a0
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2014-02-09 21:16:16 +0100

    point to files instead of directory

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,6 +1,6 @@
 cookbook_file "~/.xmodmap" do
   source ".xmodmap"
-  path "/home/#{node[:caps_lock][:user]}/"
+  path "/home/#{node[:caps_lock][:user]}/.xmodmap"
   user node[:caps_lock][:user]
   group node[:caps_lock][:user]
   mode "0644"
@@ -8,7 +8,7 @@ end
 
 cookbook_file "~/.xprofile" do
   source ".profile"
-  path "/home/#{node[:caps_lock][:user]}/"
+  path "/home/#{node[:caps_lock][:user]}/.xprofile"
   user node[:caps_lock][:user]
   group node[:caps_lock][:user]
   mode "0644"
